c5a4843dbcf24fc4b3b69a3ca3396904291bb7da
eslam ahmed fahmy <eslamfanta50080@gmail.com>
2023-05-02 20:31:37 +0300

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,189 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _showPassword = false;
+
+  StateMachineController? _controller;
+  SMIInput<bool>? _isChecking;
+  SMIInput<bool>? _isHandsUp;
+  SMIInput<bool>? _trigSuccess;
+  SMIInput<bool>? _trigFail;
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: const Color(0xffD6E2EA),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 300,
+                  width: size.width,
+                  child: RiveAnimation.asset(
+                    'assets/login.riv',
+                    stateMachines: const ["Login Machine"],
+                    onInit: (artboard) {
+                      _controller = StateMachineController.fromArtboard(
+                          artboard, "Login Machine");
+                      if (_controller == null) return;
+                      artboard.addController(_controller!);
+                      _isChecking = _controller!.findInput("isChecking")!;
+                      _isHandsUp = _controller!.findInput("isHandsUp")!;
+                      _trigSuccess = _controller!.findInput("trigSuccess")!;
+                      _trigFail = _controller!.findInput("trigFail")!;
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _emailController,
+                  onChanged: (value) {
+                    if (_isHandsUp != null) {
+                      _isHandsUp!.change(false);
+                    }
+                    if (_isChecking == null) return;
+                    _isChecking!.change(true);
+                  },
+                  onEditingComplete: () {
+                    setState(() {
+                      _isChecking!.change(false);
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    prefixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: _showPassword,
+                  onChanged: (value) {
+                    if (_isChecking != null) {
+                      _isChecking!.change(false);
+                    }
+                    if (_isHandsUp == null) return;
+                    _isHandsUp!.change(true);
+                  },
+                  onEditingComplete: () {
+                    setState(() {
+                      _isHandsUp!.change(false);
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _showPassword = !_showPassword;
+                          if (_showPassword) {
+                            _isHandsUp!.change(false);
+                          } else {
+                            _isHandsUp!.change(true);
+                          }
+                        });
+                      },
+                      icon: Icon(_showPassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Align(
+                  alignment: Alignment.centerRight,
+                  child: Text('Forgot Password?'),
+                ),
+                const SizedBox(height: 10),
+                MaterialButton(
+                  minWidth: size.width,
+                  height: 50,
+                  color: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  onPressed: () {
+                    if (_emailController.text.isNotEmpty &&
+                        _passwordController.text.isNotEmpty) {
+                      setState(() {
+                        _isChecking?.change(false);
+                        _trigSuccess?.change(true);
+                      });
+                      // Simulate a delay before navigating to the home page
+                      Timer(const Duration(seconds: 3), () {
+                        Navigator.pushNamed(context, "/HomePage");
+                      });
+                    } else {
+                      setState(() {
+                        _isChecking?.change(false);
+                        _trigFail?.change(true);
+                      });
+                    }
+                  },
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Don\'t have an account?'),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'home_page.dart';
+import 'login_page.dart';
+import 'register_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: Colors.transparent,
+              statusBarIconBrightness: Brightness.dark,
+            ),
+          ),
+        ),
+        title: "rive animation",
+        initialRoute: "/",
+        routes: {
+          "/": (context) => const LoginPage(),
+          "/HomePage": (context) => const HomePage(),
+          "/loginPage": (context) => const LoginPage(),
+          "/registerPage": (context) => const RegisterPage(),
+        },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.pushNamed(context, "/loginPage");
+              },
+              child: const Text("Login"),
+            ),
+            MaterialButton(
+              color: Colors.orange,
+              onPressed: () {
+                Navigator.pushNamed(context, "/registerPage");
+              },
+              child: const Text("Register"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
